baf27e5a66e871da26772294077d7c742eeba604
Prof. Fernando Pinéo <pf1963@fiap.com.br>
2026-03-27 09:48:14 -0300

    Realizado o deletar e editar produto no AsyncStorage, realizado o gerenciamento de estado para tratar algo no visual do app, compreendido o uso de index da FlatList ao clicar em qualquer item da lista

--- App.js
+++ App.js
@@ -1,84 +1,144 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,TextInput,FlatList,TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TextInput, FlatList, TouchableOpacity, Alert } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {useState,useEffect} from "react"
+import { useState, useEffect } from "react"
 
 export default function App() {
-  const[nomeProduto,setNomeProduto]=useState("")
-  const[precoProduto,setPrecoProduto]=useState()
-  const[listaProdutos,setListaProduto]=useState([])
+  const [nomeProduto, setNomeProduto] = useState("")
+  const [precoProduto, setPrecoProduto] = useState()
+  const [listaProdutos, setListaProduto] = useState([])
+  const [produtoEditado, setProdutoEditado] = useState(null)
+
+  //useEffect para buscar os dados salvo na AsyncStorage
+  useEffect(() => {
+    buscarProduto()//Chama a função buscar dados salvo
+  }, [])
 
   //Função para salvar produto no AsyncStorage
   async function salvar() {
     let produtos = [] //Inicializa array vazio
 
     //Verificar se já existe algum dado armazenado no AsyncStorage
-    if(await AsyncStorage.getItem("PRODUTOS")!=null){
+    if (await AsyncStorage.getItem("PRODUTOS") != null) {
       //Carrega todos os produtos no array
       produtos = JSON.parse(await AsyncStorage.getItem("PRODUTOS"))
     }
 
-    //Salva o produto no array
-    produtos.push({nomeP:nomeProduto,precoP:precoProduto})
+    if (produtoEditado) {
+      //Atualizando produto existente
+      produtos[produtoEditado.index] = { nomeP: nomeProduto, precoP: precoProduto }
+    } else {
+      //Salva o produto no array
+      produtos.push({ nomeP: nomeProduto, precoP: precoProduto })
+    }
+
 
     //Salva o produto no Async Storage
-    await AsyncStorage.setItem("PRODUTOS",JSON.stringify(produtos))
+    await AsyncStorage.setItem("PRODUTOS", JSON.stringify(produtos))
 
-    alert("PRODUTO SALVO")
+    produtoEditado?alert("PRODUTO ATUALIZADO"):alert("PRODUTO SALVO")
+
+    //Limpar o formulário...
+    setNomeProduto("")
+    setPrecoProduto("")
+    
+    //Volta o estado para null, para ficar default o cadastro
+    setProdutoEditado(null)
+
+    //Chamar a função para listar os produtos na flatlist
+    buscarProduto()
 
   }
 
   //Função para buscar produtos
   async function buscarProduto() {
-      const p = await AsyncStorage.getItem("PRODUTOS")
-      setListaProduto(JSON.parse(p))
+    const p = await AsyncStorage.getItem("PRODUTOS")
+    setListaProduto(JSON.parse(p))
+  }
+
+  //Função para deletar o produto
+  async function deletarProduto(index) {
+    const tempDados = listaProdutos
+    const dados = tempDados.filter((item, ind) => {
+      return ind !== index //Filtra o item que será excluido da lista
+    })
+
+    setListaProduto(dados)//Atualiza o estado listaProduto
+    await AsyncStorage.setItem("PRODUTOS", JSON.stringify(dados))
+  }
+
+  //Função para editar produto
+  function editarProduto(index) {
+    const produto = listaProdutos[index]
+    setNomeProduto(produto.nomeP)
+    setPrecoProduto(produto.precoP)
+    setProdutoEditado({ index })
   }
 
   return (
     <View style={styles.container}>
       <Text>CADASTRO</Text>
       {/* Campo para digitar do produto */}
-      <TextInput 
+      <TextInput
         placeholder='Digite o nome do produto'
         style={styles.input}
         value={nomeProduto}
-        onChangeText={(value)=>setNomeProduto(value)}
+        onChangeText={(value) => setNomeProduto(value)}
       />
 
       {/* Campo para digitar o preço do produto */}
-      <TextInput 
+      <TextInput
         placeholder='Digite o preço do produto'
         style={styles.input}
         value={precoProduto}
-        onChangeText={(value)=>setPrecoProduto(value)}
+        onChangeText={(value) => setPrecoProduto(value)}
       />
 
       {/* Botão para salvar o produto */}
-      <TouchableOpacity 
+      <TouchableOpacity
         style={styles.btn}
         onPress={salvar}
       >
-          <Text>CADASTRAR</Text>
+        <Text>{produtoEditado ? "ATUALIZAR" : "CADASTRAR"}</Text>
       </TouchableOpacity>
 
       {/* Botão para buscar os produtos */}
-      <TouchableOpacity 
-          style={styles.btn} 
-          onPress={buscarProduto}
+      <TouchableOpacity
+        style={styles.btn}
+        onPress={buscarProduto}
       >
-          <Text>BUSCAR DADOS</Text>
+        <Text>BUSCAR DADOS</Text>
       </TouchableOpacity>
 
       <FlatList
         data={listaProdutos}
-        renderItem={({item})=>{
-          return(
-            <>
+        ListEmptyComponent={<Text>Nenhum produto cadastrado</Text>}
+        renderItem={({ item, index }) => {
+          if (!item || !item.nomeP) return null //Garante que o item não seja null antes da renderização
+          return (
+            <View style={styles.listaItens}>
               <Text>NOME PRODUTO:{item.nomeP}</Text>
               <Text>PREÇO PRODUTO:{item.precoP}</Text>
-            </>    
-          )      
+              <View style={{ flexDirection: 'row', gap: 20 }}>
+
+                <TouchableOpacity
+                  style={styles.btnDeletar}
+                  onPress={() => deletarProduto(index)}
+                >
+                  <Text>DELETAR</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                  style={[styles.btnDeletar, { backgroundColor: "orange" }]}
+                  onPress={() => editarProduto(index)}
+                >
+                  <Text>EDITAR</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          )
         }}
+
       />
 
       <StatusBar style="auto" />
@@ -92,21 +152,36 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    gap:5
+    gap: 5
   },
-  input:{
-    borderWidth:1,
-    height:50,
-    width:300,
-    borderRadius:15
+  input: {
+    borderWidth: 1,
+    height: 50,
+    width: 300,
+    borderRadius: 15
   },
-  btn:{
-    borderWidth:1,
-    height:50,
-    width:300,
-    borderRadius:15,
-    backgroundColor:"lightblue",
-    justifyContent:"center",
-    alignItems:"center"
+  btn: {
+    borderWidth: 1,
+    height: 50,
+    width: 300,
+    borderRadius: 15,
+    backgroundColor: "lightblue",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  listaItens: {
+    width: 300,
+    borderWidth: 1,
+    borderRadius: 15,
+    height: 80,
+    alignItems: "center",
+    justifyContent: "center",
+    marginVertical: 3
+  },
+  btnDeletar: {
+    backgroundColor: "#fb9797",
+    borderRadius: 12,
+    width: 90,
+    alignItems: "center"
   }
 });
